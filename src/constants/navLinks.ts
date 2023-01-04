20a9a46e3ef894d34d00e088ea57e44928828cba
Vishwas Tamta <vishwastamta@gmail.com>
2023-01-04 20:14:55 +0530

    added navbar

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,9 @@
+export const navLinks = [
+  { id: "FALCON 9", name: "FALCON 9" },
+  { id: "FALCON HEAVY", name: "FALCON HEAVY" },
+  { id: "DRAGON", name: "DRAGON" },
+  { id: "STARSHIP", name: "STARSHIP" },
+  { id: "HUMAN SPACEFLIGHT", name: "HUMAN SPACEFLIGHT" },
+  { id: "RIDESHARE", name: "RIDESHARE" },
+  { id: "STARSHIELD", name: "STARSHIELD" },
+];
