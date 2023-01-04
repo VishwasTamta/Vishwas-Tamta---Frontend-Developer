8c03c20aa2c997bd8f4bc88b8009c285372ce91e
Vishwas Tamta <vishwastamta@gmail.com>
2023-01-04 19:11:02 +0530

    Added all the components and initial setup of tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./style";
+import { Banner, ContentList, Pagination, Search } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>NavBar</div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Banner /> <Search /> <ContentList /> <Pagination />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Banner from "./Banner";
+import ContentList from "./ContentList";
+import Pagination from "./Pagination";
+import Search from "./Search";
+
+export { Banner, ContentList, Pagination, Search };
