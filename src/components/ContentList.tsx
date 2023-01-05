1bb8b141bca50c2cddd1abd5d644dc93f9e9f8f0
Vishwas Tamta <vishwastamta@gmail.com>
2023-01-05 15:21:37 +0530

    added rockets

--- src/components/ContentList.tsx
+++ src/components/ContentList.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ContentList = () => {
-  return <div>ContentList</div>;
+  const [rockets, setrockets] = useState([] as any);
+  useEffect(() => {
+    fetch("https://api.spacexdata.com/v4/rockets")
+      .then((data) => data.json())
+      .then((data) => setrockets(data));
+
+    return () => {};
+  }, []);
+
+  return (
+    <div>
+      {rockets.map((rocket: any) => (
+        <div key={rocket.id} className="flex flex-wrap max-sm:justify-center">
+          {rocket.flickr_images.map((img: any, idx: number) => (
+            <div key={idx} className="mr-[10px] mt-[10px]">
+              <img
+                className=" rounded border w-[300px] h-[300px]"
+                src={img}
+                alt={"image"}
+              />
+              <h3 className="text-xl font-poppins text-white text-center">
+                {rocket.name}
+              </h3>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ContentList;
